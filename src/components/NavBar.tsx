d48ce7c5c8a3ff50a3b4ca703d6fe480c77e8d18
alafsasa <alafsasa@gmail.com>
2022-11-30 15:02:24 +0300

    register revamped

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,13 @@ import kwenikLogo from '../logo1000.png';
 
 export const NavBar = () => {
     return(
-        <div className='navbar navbar-expand-sm'>
+        <nav className='navbar navbar-expand-sm fixed-top'>
             <div className='container-fluid'>
                 <div className='navbar-brand'>
-                    <img src={kwenikLogo} alt="kwenikLogo"  height={50} width={50}/>
+                    <img src={kwenikLogo} alt="kwenikLogo" width={44}/>
+                        <span className='m-2' style={{fontWeight: 'bold'}}>KWENIK</span>
                 </div>
             </div>
-        </div>
+        </nav>
     );
 }
